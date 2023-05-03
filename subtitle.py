5e4c60dad01cf91719852ea41835ea0c50a8f656
pieresimbolon <129783861+pieresimbolon@users.noreply.github.com>
2023-05-03 15:14:49 +0700

    Update subtitle.py

--- subtitle.py
+++ subtitle.py
@@ -12,11 +12,11 @@ class DisplaySubtitle:
         self.root.configure(bg='red')
         self.root.overrideredirect(True)
         self.root.attributes('-alpha', 0.8)
-        self.root.attributes('-topmost', True) # Menambahkan parameter topmost
+        self.root.attributes('-topmost', True) # Menambahkan parameter 
         self.root.lift()
         self.root.wm_attributes("-disabled", True)
         self.root.wm_attributes("-transparentcolor", "black")
-        self.label = tk.Label(root, text="", font=("Lucida Grande", 20), fg="red", bg="black", wraplength=550, highlightthickness=2, highlightbackground="black")
+        self.label = tk.Label(root, text="", font=("Comic Sans MS", 20, "bold italic"), fg="white", bg="black", wraplength=550, highlightthickness=0)
         self.label.place(relx=0.5, rely=1.0, anchor='s')
         self.label.pack(side="top", fill="both", expand=True) # Letakkan label di tengah
         self.thread = threading.Thread(target=self.process_queue)
